7768fa81135efc480dfb8bddd01739777c72d987
Matheus Gusmão <mgusmao412@gmail.com>
2023-11-13 15:37:34 -0300

    🎉 feat: initial commit

---
+++ src/main/java/me/gusmao/matheus/patientscheduling/PatientSchedulingApplication.java
@@ -0,0 +1,13 @@
+package me.gusmao.matheus.patientscheduling;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PatientSchedulingApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PatientSchedulingApplication.class, args);
+	}
+
+}
---
+++ src/test/java/me/gusmao/matheus/patientscheduling/PatientSchedulingApplicationTests.java
@@ -0,0 +1,13 @@
+package me.gusmao.matheus.patientscheduling;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PatientSchedulingApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
