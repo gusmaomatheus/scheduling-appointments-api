da9221c0c634fb5a3de95e2e0a79b9f2ede92207
Matheus Gusmão <mgusmao412@gmail.com>
2023-11-14 02:35:01 -0300

    🎉 feat: created a PatientController#save post endpoint

--- src/main/java/me/gusmao/matheus/patientscheduling/controllers/PatientController.java
+++ src/main/java/me/gusmao/matheus/patientscheduling/controllers/PatientController.java
@@ -1,7 +1,12 @@
 package me.gusmao.matheus.patientscheduling.controllers;
 
 import lombok.RequiredArgsConstructor;
+import me.gusmao.matheus.patientscheduling.dtos.PatientDTO;
+import me.gusmao.matheus.patientscheduling.entities.Patient;
 import me.gusmao.matheus.patientscheduling.services.patient.PatientService;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -11,4 +16,11 @@ import org.springframework.web.bind.annotation.RestController;
 public class PatientController {
 
     private final PatientService service;
+
+    @PostMapping
+    public ResponseEntity<Patient> save(@RequestBody PatientDTO data) {
+        Patient patient = this.service.save(data);
+
+        return ResponseEntity.status(201).body(patient);
+    }
 }
