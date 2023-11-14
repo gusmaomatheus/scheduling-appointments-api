2949b23e7cb6a256c8d34427b3bf2b22670ba44b
Matheus Gusmão <mgusmao412@gmail.com>
2023-11-14 00:55:48 -0300

    🎉 feat: created Schedule entity

---
+++ src/main/java/me/gusmao/matheus/patientscheduling/entities/Schedule.java
@@ -0,0 +1,28 @@
+package me.gusmao.matheus.patientscheduling.entities;
+
+import com.fasterxml.jackson.annotation.JsonFormat;
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import org.hibernate.annotations.CreationTimestamp;
+
+import java.time.LocalDateTime;
+import java.util.UUID;
+
+@Entity
+@Table(name = "schedule")
+@NoArgsConstructor
+@AllArgsConstructor
+@Data
+public class Schedule {
+
+    @Id @GeneratedValue(strategy = GenerationType.UUID)
+    private UUID id;
+    private String description;
+    private LocalDateTime date;
+    @CreationTimestamp @JsonFormat(pattern = "dd/MM/yyyy HH:mm:ss")
+    private LocalDateTime createdAt;
+    @ManyToOne
+    private Patient patient;
+}
