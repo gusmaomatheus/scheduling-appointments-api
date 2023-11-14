c690007bfd9851fdec0ff9e966f22dda69470d22
Matheus Gusmão <mgusmao412@gmail.com>
2023-11-14 02:05:02 -0300

    🎉 feat: created a PatientService#save for save an entity

--- src/main/java/me/gusmao/matheus/patientscheduling/services/patient/PatientService.java
+++ src/main/java/me/gusmao/matheus/patientscheduling/services/patient/PatientService.java
@@ -1,4 +1,9 @@
 package me.gusmao.matheus.patientscheduling.services.patient;
 
+import me.gusmao.matheus.patientscheduling.dto.PatientDTO;
+import me.gusmao.matheus.patientscheduling.entities.Patient;
+
 public interface PatientService {
+
+    Patient save(PatientDTO data);
 }
--- src/main/java/me/gusmao/matheus/patientscheduling/services/patient/PatientServiceImpl.java
+++ src/main/java/me/gusmao/matheus/patientscheduling/services/patient/PatientServiceImpl.java
@@ -1,12 +1,23 @@
 package me.gusmao.matheus.patientscheduling.services.patient;
 
+import lombok.RequiredArgsConstructor;
+import me.gusmao.matheus.patientscheduling.dto.PatientDTO;
+import me.gusmao.matheus.patientscheduling.entities.Patient;
 import me.gusmao.matheus.patientscheduling.repositories.PatientRepository;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
 @Service
+@Transactional
+@RequiredArgsConstructor
 public class PatientServiceImpl implements PatientService {
 
-    @Autowired
-    private PatientRepository patientRepository;
+    private final PatientRepository repository;
+
+    @Override
+    public Patient save(PatientDTO data) {
+        Patient patient = new Patient(data);
+
+        return this.repository.save(patient);
+    }
 }
