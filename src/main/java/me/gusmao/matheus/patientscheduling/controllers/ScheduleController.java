98e07f439cdcd76173e9d10ab7a6b37e3ab9ad54
Matheus Gusmão <mgusmao412@gmail.com>
2023-11-15 16:52:12 -0300

    🎉 feat: created a ScheduleController#save post endpoint

--- src/main/java/me/gusmao/matheus/patientscheduling/controllers/ScheduleController.java
+++ src/main/java/me/gusmao/matheus/patientscheduling/controllers/ScheduleController.java
@@ -1,7 +1,12 @@
 package me.gusmao.matheus.patientscheduling.controllers;
 
 import lombok.RequiredArgsConstructor;
+import me.gusmao.matheus.patientscheduling.dtos.ScheduleDTO;
+import me.gusmao.matheus.patientscheduling.entities.Schedule;
 import me.gusmao.matheus.patientscheduling.services.schedule.ScheduleServiceImpl;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -12,4 +17,10 @@ public class ScheduleController {
 
     private final ScheduleServiceImpl service;
 
+    @PostMapping
+    public ResponseEntity<Schedule> save(@RequestBody ScheduleDTO data) {
+        Schedule schedule = this.service.save(data);
+
+        return ResponseEntity.status(201).body(schedule);
+    }
 }
