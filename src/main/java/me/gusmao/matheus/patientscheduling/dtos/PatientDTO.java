c3901919fae51b39a98a3f6edbe289f235233f31
Matheus Gusmão <mgusmao412@gmail.com>
2023-11-15 07:55:51 -0300

    🎉 feat: added more validations

--- src/main/java/me/gusmao/matheus/patientscheduling/dtos/PatientDTO.java
+++ src/main/java/me/gusmao/matheus/patientscheduling/dtos/PatientDTO.java
@@ -2,13 +2,16 @@ package me.gusmao.matheus.patientscheduling.dtos;
 
 import jakarta.validation.constraints.Email;
 import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Size;
 import org.hibernate.validator.constraints.br.CPF;
 
 public record PatientDTO(
 
         @NotBlank(message = "campo 'firstName' não pode ser vazio.")
+        @Size(min = 5, max = 30, message = "campo 'fristName' deve ter entre 5 e 30 caracteres.")
         String firstName,
         @NotBlank(message = "campo 'lastName' não pode ser vazio.")
+        @Size(min = 5, max = 30, message = "campo 'lastName' deve ter entre 5 e 60 caracteres.")
         String lastName,
         @CPF(message = "formato inválido para o campo 'cpf'.")
         String cpf,
