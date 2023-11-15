4a9e17ca06a4a9274bb5509932db09959618383b
Matheus Gusmão <mgusmao412@gmail.com>
2023-11-15 16:47:44 -0300

    🎉 feat: reformed ScheduleDTO data-transfer-object

--- src/main/java/me/gusmao/matheus/patientscheduling/dtos/ScheduleDTO.java
+++ src/main/java/me/gusmao/matheus/patientscheduling/dtos/ScheduleDTO.java
@@ -1,7 +1,10 @@
 package me.gusmao.matheus.patientscheduling.dtos;
 
+import jakarta.validation.constraints.Future;
 import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
 import jakarta.validation.constraints.Size;
+import org.springframework.format.annotation.DateTimeFormat;
 
 import java.time.LocalDateTime;
 
@@ -9,6 +12,10 @@ public record ScheduleDTO(
         @NotBlank(message = "O campo 'description' não pode ser vazio.") @Size(min = 10, max = 255, message = "A descrição deve conter entre 10 e 255 caracteres.")
         String description,
         @NotBlank(message = "O campo 'date' não pode ser vazio.")
-        LocalDateTime date
+        @Future
+        @DateTimeFormat(pattern = "yy-MM-ddTHH:mm:ss")
+        LocalDateTime date,
+        @NotNull(message = "O campo 'patientId' não pode ser nulo.")
+        Long patientId
 ) {
 }
