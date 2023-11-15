039e5ff1d0321879f6e00ceeb17bc8f1ee42d8aa
Matheus Gusmão <mgusmao412@gmail.com>
2023-11-15 17:22:58 -0300

    🎉 feat: created ScheduleController#update put endpoint

--- src/main/java/me/gusmao/matheus/patientscheduling/controllers/ScheduleController.java
+++ src/main/java/me/gusmao/matheus/patientscheduling/controllers/ScheduleController.java
@@ -1,5 +1,6 @@
 package me.gusmao.matheus.patientscheduling.controllers;
 
+import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
 import me.gusmao.matheus.patientscheduling.dtos.ScheduleDTO;
 import me.gusmao.matheus.patientscheduling.entities.Schedule;
@@ -17,7 +18,7 @@ public class ScheduleController {
     private final ScheduleServiceImpl service;
 
     @PostMapping
-    public ResponseEntity<Schedule> save(@RequestBody ScheduleDTO data) {
+    public ResponseEntity<Schedule> save(@Valid @RequestBody ScheduleDTO data) {
         Schedule schedule = this.service.save(data);
 
         return ResponseEntity.status(201).body(schedule);
@@ -31,9 +32,17 @@ public class ScheduleController {
     }
 
     @GetMapping("/{id}")
-
     public ResponseEntity<Schedule> findById(@PathVariable Long id) {
         Schedule schedule = this.service.findById(id);
+
+        return ResponseEntity.status(200).body(schedule);
+    }
+
+    @PutMapping("/{id}")
+    public ResponseEntity update(@PathVariable Long id, @Valid @RequestBody ScheduleDTO data) {
+        Schedule schedule = this.service.findById(id);
+
+        this.service.update(id, data);
 
         return ResponseEntity.status(200).body(schedule);
     }
