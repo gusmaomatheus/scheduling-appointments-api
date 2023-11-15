f98461e8fd35d86c84bd89d4f5da0927cb65b031
Matheus Gusmão <mgusmao412@gmail.com>
2023-11-15 16:51:45 -0300

    🎉 feat: created ScheduleService#save for save an entity

--- src/main/java/me/gusmao/matheus/patientscheduling/services/schedule/ScheduleService.java
+++ src/main/java/me/gusmao/matheus/patientscheduling/services/schedule/ScheduleService.java
@@ -1,5 +1,9 @@
 package me.gusmao.matheus.patientscheduling.services.schedule;
 
+import me.gusmao.matheus.patientscheduling.dtos.ScheduleDTO;
+import me.gusmao.matheus.patientscheduling.entities.Schedule;
+
 public interface ScheduleService {
 
+    Schedule save(ScheduleDTO data);
 }
--- src/main/java/me/gusmao/matheus/patientscheduling/services/schedule/ScheduleServiceImpl.java
+++ src/main/java/me/gusmao/matheus/patientscheduling/services/schedule/ScheduleServiceImpl.java
@@ -1,6 +1,8 @@
 package me.gusmao.matheus.patientscheduling.services.schedule;
 
 import lombok.RequiredArgsConstructor;
+import me.gusmao.matheus.patientscheduling.dtos.ScheduleDTO;
+import me.gusmao.matheus.patientscheduling.entities.Schedule;
 import me.gusmao.matheus.patientscheduling.mappers.ScheduleMapper;
 import me.gusmao.matheus.patientscheduling.repositories.ScheduleRepository;
 import org.springframework.stereotype.Service;
@@ -14,4 +16,10 @@ public class ScheduleServiceImpl implements ScheduleService {
     private final ScheduleRepository repository;
     private final ScheduleMapper mapper;
 
+    @Override
+    public Schedule save(ScheduleDTO data) {
+        Schedule schedule = mapper.transform(data);
+
+        return this.repository.save(schedule);
+    }
 }
