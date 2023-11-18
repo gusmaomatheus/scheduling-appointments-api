710129144a670ad17ad1f2ce2f3bda353993f423
Matheus Gusmão <mgusmao412@gmail.com>
2023-11-18 19:06:24 -0300

    🎉 feat: created SecurityFilter for validate a user token

---
+++ src/main/java/me/gusmao/matheus/patientscheduling/config/SecurityFilter.java
@@ -0,0 +1,49 @@
+package me.gusmao.matheus.patientscheduling.config;
+
+import jakarta.servlet.FilterChain;
+import jakarta.servlet.ServletException;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import lombok.RequiredArgsConstructor;
+import me.gusmao.matheus.patientscheduling.repositories.UserRepository;
+import me.gusmao.matheus.patientscheduling.services.auth.AuthTokenService;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.stereotype.Component;
+import org.springframework.web.filter.OncePerRequestFilter;
+
+import java.io.IOException;
+
+@Component
+@RequiredArgsConstructor
+public class SecurityFilter extends OncePerRequestFilter {
+
+    private final AuthTokenService tokenService;
+    private final UserRepository userRepository;
+
+    @Override
+    protected void doFilterInternal(HttpServletRequest request, HttpServletResponse response, FilterChain filterChain) throws ServletException, IOException {
+        String token = this.recoverToken(request);
+
+        if (token != null) {
+            String login = this.tokenService.validateToken(token);
+            UserDetails user = this.userRepository.findByLogin(login);
+
+            UsernamePasswordAuthenticationToken authentication = new UsernamePasswordAuthenticationToken(user, null, user.getAuthorities());
+            SecurityContextHolder.getContext().setAuthentication(authentication);
+        }
+
+        filterChain.doFilter(request, response);
+    }
+
+    private String recoverToken(HttpServletRequest request) {
+        String authHeader = request.getHeader("Authorization");
+
+        if (authHeader == null) {
+            return null;
+        }
+
+        return authHeader.replace("Bearer ", "");
+    }
+}
