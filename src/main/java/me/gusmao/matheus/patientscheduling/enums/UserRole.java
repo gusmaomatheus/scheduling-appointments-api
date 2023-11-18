77e388302a133ee09e81110cd17a00f75f3c5d39
Matheus Gusmão <mgusmao412@gmail.com>
2023-11-18 15:09:10 -0300

    🎉 feat: created UserRole enum to define application roles

---
+++ src/main/java/me/gusmao/matheus/patientscheduling/enums/UserRole.java
@@ -0,0 +1,16 @@
+package me.gusmao.matheus.patientscheduling.enums;
+
+public enum UserRole {
+    ADMIN("admin"),
+    USER("user");
+
+    private String role;
+
+    UserRole(String role) {
+        this.role = role;
+    }
+
+    public String getRole() {
+        return this.role;
+    }
+}
