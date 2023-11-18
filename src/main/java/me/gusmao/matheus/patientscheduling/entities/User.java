6ebaec4e648ddd21b3d1eb05a48eb09ab4af7db8
Matheus Gusmão <mgusmao412@gmail.com>
2023-11-18 16:00:04 -0300

    🎉 feat: created a new constructor for User

--- src/main/java/me/gusmao/matheus/patientscheduling/entities/User.java
+++ src/main/java/me/gusmao/matheus/patientscheduling/entities/User.java
@@ -25,6 +25,12 @@ public class User implements UserDetails {
     private String password;
     private UserRole role;
 
+    public User(String login, String password, UserRole role) {
+        this.login = login;
+        this.password = password;
+        this.role = role;
+    }
+
     @Override
     public Collection<? extends GrantedAuthority> getAuthorities() {
         if (this.role == UserRole.ADMIN) {
