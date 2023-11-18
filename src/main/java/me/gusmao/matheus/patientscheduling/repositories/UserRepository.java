9e98318d33966c79145c14caa5be03604a6df699
Matheus Gusmão <mgusmao412@gmail.com>
2023-11-18 15:09:27 -0300

    🎉 feat: created UserRepository repository

---
+++ src/main/java/me/gusmao/matheus/patientscheduling/repositories/UserRepository.java
@@ -0,0 +1,12 @@
+package me.gusmao.matheus.patientscheduling.repositories;
+
+import me.gusmao.matheus.patientscheduling.entities.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface UserRepository extends JpaRepository<User, Long> {
+
+    UserDetails findByLogin(String login);
+}
