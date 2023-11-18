fe106fd28f4e913e2b9c549fceadc9ad278f9bdb
Matheus Gusmão <mgusmao412@gmail.com>
2023-11-18 19:05:23 -0300

    🎉 feat: changed the text size for login and password

--- src/main/java/me/gusmao/matheus/patientscheduling/dtos/auth/LoginDTO.java
+++ src/main/java/me/gusmao/matheus/patientscheduling/dtos/auth/LoginDTO.java
@@ -5,10 +5,8 @@ import jakarta.validation.constraints.Size;
 
 public record LoginDTO(
         @NotBlank(message = "O campo 'login' não pode ser vazio.")
-        @Size(message = "O login deve ter entre 10 e 50 caracteres.", min = 10, max = 50)
         String login,
         @NotBlank(message = "O campo 'password' não pode ser vazio.")
-        @Size(message = "A senha deve ter entre 6 e 16 caracteres.", min = 6, max = 16)
         String password
 ) {
 }
--- src/main/java/me/gusmao/matheus/patientscheduling/dtos/auth/RegisterDTO.java
+++ src/main/java/me/gusmao/matheus/patientscheduling/dtos/auth/RegisterDTO.java
@@ -7,10 +7,10 @@ import me.gusmao.matheus.patientscheduling.enums.UserRole;
 
 public record RegisterDTO(
         @NotBlank(message = "O campo 'login' não pode ser vazio.")
-        @Size(message = "O login deve ter entre 10 e 50 caracteres.", min = 10, max = 50)
+        @Size(message = "O login deve ter entre 10 e 50 caracteres.", min = 5, max = 50)
         String login,
         @NotBlank(message = "O campo 'password' não pode ser vazio.")
-        @Size(message = "A senha deve ter entre 6 e 16 caracteres.", min = 6, max = 16)
+        @Size(message = "A senha deve ter entre 6 e 16 caracteres.", min = 5, max = 16)
         String password,
         @NotNull(message = "O campo 'role' não pode ser nulo.")
         UserRole role
