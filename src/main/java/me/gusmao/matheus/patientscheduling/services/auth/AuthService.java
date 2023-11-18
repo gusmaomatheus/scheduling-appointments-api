ae1dc920e623e26a133dfa2bd206f35801fbf4b8
Matheus Gusmão <mgusmao412@gmail.com>
2023-11-18 15:51:42 -0300

    🎉 feat: created AuthService#login to authenticate a user

--- src/main/java/me/gusmao/matheus/patientscheduling/services/auth/AuthService.java
+++ src/main/java/me/gusmao/matheus/patientscheduling/services/auth/AuthService.java
@@ -1,4 +1,8 @@
 package me.gusmao.matheus.patientscheduling.services.auth;
 
+import me.gusmao.matheus.patientscheduling.dtos.auth.LoginDTO;
+
 public interface AuthService {
+
+    void login(LoginDTO data);
 }
--- src/main/java/me/gusmao/matheus/patientscheduling/services/auth/AuthServiceImpl.java
+++ src/main/java/me/gusmao/matheus/patientscheduling/services/auth/AuthServiceImpl.java
@@ -1,7 +1,10 @@
 package me.gusmao.matheus.patientscheduling.services.auth;
 
 import lombok.RequiredArgsConstructor;
+import me.gusmao.matheus.patientscheduling.dtos.auth.LoginDTO;
 import me.gusmao.matheus.patientscheduling.repositories.UserRepository;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.core.userdetails.UsernameNotFoundException;
@@ -12,9 +15,16 @@ import org.springframework.stereotype.Service;
 public class AuthServiceImpl implements AuthService, UserDetailsService {
 
     private final UserRepository repository;
+    private final AuthenticationManager authManager;
 
     @Override
     public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
         return this.repository.findByLogin(username);
+    }
+
+    @Override
+    public void login(LoginDTO data) {
+        var usernamePassword = new UsernamePasswordAuthenticationToken(data.login(), data.password());
+        var auth = this.authManager.authenticate(usernamePassword);
     }
 }
