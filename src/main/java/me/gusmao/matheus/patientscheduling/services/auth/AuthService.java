e4c7ac062066e871eb3541153e5c136e53c8e074
Matheus Gusmão <mgusmao412@gmail.com>
2023-11-18 16:06:59 -0300

    🎉 feat: created AuthService#login to register a user

--- src/main/java/me/gusmao/matheus/patientscheduling/services/auth/AuthService.java
+++ src/main/java/me/gusmao/matheus/patientscheduling/services/auth/AuthService.java
@@ -1,8 +1,11 @@
 package me.gusmao.matheus.patientscheduling.services.auth;
 
 import me.gusmao.matheus.patientscheduling.dtos.auth.LoginDTO;
+import me.gusmao.matheus.patientscheduling.dtos.auth.RegisterDTO;
 
 public interface AuthService {
+
+    void register(RegisterDTO data);
 
     void login(LoginDTO data);
 }
--- src/main/java/me/gusmao/matheus/patientscheduling/services/auth/AuthServiceImpl.java
+++ src/main/java/me/gusmao/matheus/patientscheduling/services/auth/AuthServiceImpl.java
@@ -2,12 +2,15 @@ package me.gusmao.matheus.patientscheduling.services.auth;
 
 import lombok.RequiredArgsConstructor;
 import me.gusmao.matheus.patientscheduling.dtos.auth.LoginDTO;
+import me.gusmao.matheus.patientscheduling.dtos.auth.RegisterDTO;
+import me.gusmao.matheus.patientscheduling.entities.User;
 import me.gusmao.matheus.patientscheduling.repositories.UserRepository;
 import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.stereotype.Service;
 
 @Service
@@ -20,6 +23,14 @@ public class AuthServiceImpl implements AuthService, UserDetailsService {
     @Override
     public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
         return this.repository.findByLogin(username);
+    }
+
+    @Override
+    public void register(RegisterDTO data) {
+        String encryptedPassword = new BCryptPasswordEncoder().encode(data.password());
+        User user = new User(data.login(), encryptedPassword, data.role());
+
+        this.repository.save(user);
     }
 
     @Override
