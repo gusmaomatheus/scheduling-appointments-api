05c712fcc028b45928439ab00c43f5ff10e8ef2b
Matheus Gusmão <mgusmao412@gmail.com>
2023-11-18 22:15:29 -0300

    🎉 feat: created PatientRepository#findByCpf for get a patient by your id

--- src/main/java/me/gusmao/matheus/patientscheduling/repositories/PatientRepository.java
+++ src/main/java/me/gusmao/matheus/patientscheduling/repositories/PatientRepository.java
@@ -4,6 +4,10 @@ import me.gusmao.matheus.patientscheduling.models.entities.Patient;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.Optional;
+
 @Repository
 public interface PatientRepository extends JpaRepository<Patient, Long> {
+
+    Optional<Patient> findByCpf(String cpf);
 }
