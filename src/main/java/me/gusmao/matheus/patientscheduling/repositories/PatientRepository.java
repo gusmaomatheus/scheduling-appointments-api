18dcabf754486f925251692d528c544f94ed783d
Matheus Gusmão <mgusmao412@gmail.com>
2023-11-19 00:12:33 -0300

    🎉 feat: created PatientRepository#findByEmail for get a patient by your email

--- src/main/java/me/gusmao/matheus/patientscheduling/repositories/PatientRepository.java
+++ src/main/java/me/gusmao/matheus/patientscheduling/repositories/PatientRepository.java
@@ -10,4 +10,6 @@ import java.util.Optional;
 public interface PatientRepository extends JpaRepository<Patient, Long> {
 
     Optional<Patient> findByCpf(String cpf);
+
+    Optional<Patient> findByEmail(String email);
 }
