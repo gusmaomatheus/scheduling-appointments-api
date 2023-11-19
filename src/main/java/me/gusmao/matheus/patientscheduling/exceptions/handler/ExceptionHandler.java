8a145a5244460a28987473e0ac791c86568db50b
Matheus Gusmão <mgusmao412@gmail.com>
2023-11-18 22:22:36 -0300

    🎉 feat: created ExceptionResponse and ExceptionHandler to handle exceptions in a personalized way

---
+++ src/main/java/me/gusmao/matheus/patientscheduling/exceptions/handler/ExceptionHandler.java
@@ -0,0 +1,7 @@
+package me.gusmao.matheus.patientscheduling.exceptions.handler;
+
+import org.springframework.web.bind.annotation.ControllerAdvice;
+
+@ControllerAdvice
+public class ExceptionHandler {
+}
---
+++ src/main/java/me/gusmao/matheus/patientscheduling/exceptions/model/ExceptionResponse.java
@@ -0,0 +1,16 @@
+package me.gusmao.matheus.patientscheduling.exceptions.model;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.time.LocalDateTime;
+
+@AllArgsConstructor
+@NoArgsConstructor
+@Data
+public class ExceptionResponse {
+    private String message;
+    private String status;
+    private LocalDateTime dateOccurrence;
+}
