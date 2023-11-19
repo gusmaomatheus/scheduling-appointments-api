befe40d02e910ce50b518d7372391c5b4880ddc8
Matheus Gusmão <mgusmao412@gmail.com>
2023-11-18 22:33:06 -0300

    🎉 feat: renamed ExceptionHandler to CustomExceptionHandler because of a conflict with @ExceptionHandler spring boot annotation

--- src/main/java/me/gusmao/matheus/patientscheduling/exceptions/handler/CustomExceptionHandler.java
+++ src/main/java/me/gusmao/matheus/patientscheduling/exceptions/handler/CustomExceptionHandler.java
@@ -3,5 +3,7 @@ package me.gusmao.matheus.patientscheduling.exceptions.handler;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 
 @ControllerAdvice
-public class ExceptionHandler {
+public class CustomExceptionHandler {
+
+
 }
