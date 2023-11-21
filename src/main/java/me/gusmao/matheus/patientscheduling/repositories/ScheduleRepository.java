92f74b3e635b04b6e65c2fcefca9622b53f78fec
Matheus Gusmão <mgusmao412@gmail.com>
2023-11-20 23:11:49 -0300

    🎉 feat: created ScheduleRepository#findByDate for get a schedule by your date.

--- src/main/java/me/gusmao/matheus/patientscheduling/repositories/ScheduleRepository.java
+++ src/main/java/me/gusmao/matheus/patientscheduling/repositories/ScheduleRepository.java
@@ -2,8 +2,16 @@ package me.gusmao.matheus.patientscheduling.repositories;
 
 import me.gusmao.matheus.patientscheduling.models.entities.Schedule;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
+
+import java.time.LocalDateTime;
+import java.util.Optional;
 
 @Repository
 public interface ScheduleRepository extends JpaRepository<Schedule, Long> {
+
+    @Query("SELECT schedule FROM Schedule schedule WHERE schedule.date = :date")
+    Optional<Schedule> findByDate(@Param("date") LocalDateTime date);
 }
